fe677f3e5811cb4f6b9c4dfae3a966af45650ec5
Taylor L Money <git@taylorlmoney.com>
2020-06-22 20:53:54 -0700

    Create

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+    with open("README.md", "r") as fh:
+        long_description = fh.read()
+
+    setuptools.setup(
+        name="session",
+        version="0.0.1",
+        author="Taylor L Money",
+        author_email="git@taylorlmoney.com",
+        description="",
+        long_description=open(README.md).read()
+        long_description_content_type="text/markdown",
+        url="https://github.com/youngmoney/session",
+        packages=setuptools.find_packages(),
+        install_requires=["Flask", "requests"],
+        entry_points={
+            "console_scripts": [
+                "session = session.cli::parse_and_run"
+            ]
+        },
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+        python_requires=">=3.6",
+    )
